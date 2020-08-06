bedb33b831158af66fdce61431d91165002665f1
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-06 15:55:54 +0700

    Fetch API - News by Categories

--- src/alanStudio.js
+++ src/alanStudio.js
@@ -54,5 +54,36 @@ intent('what\'s up with $(term* (.*))', (p) => {
     })
 })
 
+// News by Categories
+const CATEGORIES = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+const CATEGORIES_INTENT = `${CATEGORIES.map((category) => `${category}~${category}`).join('|')}|`;
+
+intent(`(show|what is|tell me|what's|what are|what're|read) (the|) (recent|latest|) $(N news|headlines) (in|about|on|) $(C~ ${CATEGORIES_INTENT})`,
+  `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`, (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`
+    
+    if(p.C.value){
+        NEWS_API_URL = `${NEWS_API_URL}&category=${p.C.value}`
+    }
+    
+    api.request(NEWS_API_URL, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length){
+            p.play('Sorry, please try searching for a different category.');
+            return;
+        }
+        
+        savedArticles = articles;
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|recent) articles on ${p.C.values}.`);
+        
+    })
+})
+
+
+
+
 
 */
