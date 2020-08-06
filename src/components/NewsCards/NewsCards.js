c87825c8d6ff4a28c5dd8e4de2cd0ee81fe67e39
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-06 18:09:45 +0700

    Add unique key

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -46,6 +46,7 @@ const NewsCards = ({ articles, activeArticle }) => {
               md={4}
               lg={3}
               className={classes.infoCard}
+              key={infoCard.title}
             >
               <div
                 className={classes.card}
@@ -80,7 +81,12 @@ const NewsCards = ({ articles, activeArticle }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
-            <NewsCard article={article} activeArticle={activeArticle} i={i} />
+            <NewsCard
+              key={i}
+              article={article}
+              activeArticle={activeArticle}
+              i={i}
+            />
           </Grid>
         ))}
       </Grid>
