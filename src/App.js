1b3064d29da20be4c51c0d4475e5dc3c18b05d05
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-06 12:51:01 +0700

    add alanBtn to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "73c57b3ea73e11319e1cca017e02a1bd2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log("articles", articles);
+        }
+      },
+    });
+  }, []);
   return (
     <div className="App">
       <h1>Hello world</h1>
