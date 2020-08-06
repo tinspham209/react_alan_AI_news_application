c5c139852cfe40776318d935c913c44fac67eda3
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-06 14:28:05 +0700

    Add logo to HomePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./App-style.js";
 
 const alanKey =
   "73c57b3ea73e11319e1cca017e02a1bd2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -19,8 +21,14 @@ function App() {
     });
   }, []);
   return (
-    <div className="App">
-      <h1>Alan AI News Application</h1>
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
